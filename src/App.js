a20cc5afd350821faf64e0b686863035ab26f5d8
IMRamAgrawal <imramagrawal@gmail.com>
2024-03-11 22:38:35 +0530

    functional programming

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import { BookInfo } from "./components/book-info";
-import { UserInfo } from "./components/user-info";
+import { Recursive } from "./components/recursive";
+// import { SmallButton, SmallRedButton } from "./components/partial";
+import { LargeRedButton, SmallButton } from "./components/partial";
+const myNestedObject = {
+  key1: "value1",
+  key2: {
+    innerKey1: "innerValue1",
+    innerKey2: {
+      innerInnerKey1: "innerInnerValue1",
+      innerInnerKey2: "innerInnerValue2",
+    },
+  },
+  key3: "value3",
+};
 
 function App() {
   return (
     <>
-      <UserInfo userId={"1"}/>
-      <BookInfo bookId={"2"}/>
+      {/* <Recursive data={myNestedObject} /> */}
+      {/* <SmallButton text={"I am small!"}/>
+      <SmallRedButton text={"I am small and Red"}/> */}
+      <SmallButton text={"I am small!"}/>
+      <LargeRedButton text="I am large and Red"/>
     </>
   );
 }
