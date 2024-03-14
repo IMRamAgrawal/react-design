8cf8b97f5ca7916924d6aac47e158b4b711ce949
IMRamAgrawal <imramagrawal@gmail.com>
2024-03-14 16:46:46 +0530

    useId

--- src/App.js
+++ src/App.js
@@ -1,37 +1,14 @@
-import { useState, useRef, useEffect } from "react";
-import "./App.css";
+import Form from "./input";
 
 function App() {
-  const [show, setShow] = useState(false);
-  const [top, setTop] = useState(0);
-  const buttonRef = useRef(null);
-
-  useEffect(() => {
-    if (buttonRef.current === null || !show) return setTop(0);
-    const { bottom } = buttonRef.current.getBoundingClientRect();
-    setTop(bottom + 30);
-  }, [show]);
-
-  const now = performance.now();
-  while (now > performance.now() - 100) {
-    //Do something
-  }
-
   return (
     <>
-      <button ref={buttonRef} onClick={() => setShow((s) => !s)}>
-        Show
-      </button>
-      {show && (
-        <div
-          className="tooltip"
-          style={{
-            top: `${top}px`,
-          }}
-        >
-          Some text ...
-        </div>
-      )}
+      <Form />
+      <p>
+        It is a long established fact that a reader will be distracted by the
+        readable content of a page when looking at its layout.
+      </p>
+      <Form />
     </>
   );
 }
