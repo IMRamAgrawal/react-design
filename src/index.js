8899ff4cdbc9edcb3d2dabe7ff73f8a951c2f7b1
IMRamAgrawal <imramagrawal@gmail.com>
2024-03-15 22:59:19 +0530

    context api

--- src/index.js
+++ src/index.js
@@ -1,25 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Nav from "./components/nav";
-import { mainRoute } from "./components/main";
-import { booksRoute } from "./components/books";
-import Club from "./components/club";
-
-const router = createBrowserRouter([
-  {
-    element: <Nav />,
-    children: [
-      { index: true, ...mainRoute },
-      { path: "/books", ...booksRoute },
-      { path: "/club", element: <Club /> },
-    ],
-  },
-]);
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
