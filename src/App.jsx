26334fcc10e6ce8b52da1e070d280657bd5117a8
IMRamAgrawal <imramagrawal@gmail.com>
2024-03-18 15:44:30 +0530

    Throttling

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Ingredients from "./ingredientsComponents/ingredients";
 import IngredientsInfoHelper from "./ingredientsComponents/ingredients-info-helper";
+import TrackMouse from "./components/track-mouse";
 
 function App() {
   return (
     <>
-      <Ingredients IngredientsInfoHelper={<IngredientsInfoHelper/>}/>
+      {/* <Ingredients IngredientsInfoHelper={<IngredientsInfoHelper/>}/> */}
+      <TrackMouse />
     </>
   );
 }
