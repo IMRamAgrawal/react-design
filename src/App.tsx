253bee4bd5e8f2aff8fb268f4892c58523633e1d
IMRamAgrawal <imramagrawal@gmail.com>
2024-03-21 16:13:11 +0530

    complex states

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,18 @@
-// import { useEffect, useState } from "react";
-// import axios from "axios";
-// import Loader from "./components/loader/loader";
-// import Book from "./components/books/book";
-// import Books from "./components/books/books";
-// import "./App.css";
-
-// export type Book = {
-//   id: number;
-//   title: string;
-//   author?: string;
-// };
-
-// export const fetchBooks = async (count: number) => {
-//   const response = await axios.get(`/api/books?limit=${count}`);
-//   return response.data;
-// };
-
-// function App() {
-//   const [books, setBooks] = useState<Book[]>([]);
-//   const [count, setCount] = useState(10);
-
-//   return (
-//     <main className="w-full max-w-2xl py-16 mx-auto">
-//       <Books setBooks={}
-//         // count={count}
-//         // onChange={(e) => setCount(e.target.valueAsNumber)}
-//         // onSubmit={() => fetchBooks(count).then(setBooks)}
-//       >
-//         {books.map((book) => {
-//           return <Book title={book.title} author={book.author} />;
-//         })}
-//       </Books>
-//     </main>
-//   );
-// }
-
-// export default App;
-import { useRef, useState } from "react";
+import { useUser } from "./components/use-user";
 
 function App() {
-  const inputRef = useRef<HTMLInputElement>(null);
-  const mutableRefValue = useRef(20);
+  const [status, value] = useUser<{ name: string }>("/user");
 
-  mutableRefValue.current = 20;
+  if (status === "fetching") {
+    return <div>Fetching...</div>;
+  }
 
-  return (
-    <>
-      <input ref={inputRef} />
-    </>
-  );
+  if (status === "error") {
+    return <div>Error: {value.message}</div>;
+  }
+
+  return <div>{value.name}</div>;
 }
 
 export default App;
+
